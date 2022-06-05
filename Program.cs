9c923d358f70214ef718ac424eeee295048e92aa
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:57:02 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double n1 = 10;
+double n2 = 100;
+double v;
+
+Console.WriteLine("Calcular a subtração de dois valores!!!");
+
+v = n1 - n2;
+
+if( v < 0){
+    Console.WriteLine($"Valor negativo! O valor é: {v}");
+}else if(v > 0){
+    Console.WriteLine($"Valor positivo! O valor é: {v}");
+}else {
+    Console.WriteLine("Valor Zero!");
+}
